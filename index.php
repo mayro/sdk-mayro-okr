a6ec552113bd1dbcbfbe45ad3eb4b56204484c08
Amira Jelassi <jelassi.amira85@gmail.com>
2020-09-18 18:37:54 +0200

    init

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
             document.getElementById('bonjour-la-vie').style.visibility = visibility;
             document.getElementById('link-okr').style.color = color;
             document.getElementById('link-okr').style.textAlign = align;
-
+            document.getElementById('btn-sign-id').style.color = color;
         });
     </script>
 
@@ -84,7 +84,7 @@
                     </ul>
                 </li>
                 <li><a href="contact.html">Contact</a></li>
-                <li><a class="btn" href="signin.html" onclick="sendEvent()">SIGN IN / SIGN UP</a></li>
+                <li><a class="btn" id = "btn-sign-id" href="signin.html" onclick="sendEvent()">SIGN IN / SIGN UP</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
